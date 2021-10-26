0b7bd4f406c42ab85f94a703f21505136c656f54
Alina <tahera.firdose@gmail.com>
2021-10-26 22:34:18 +0530

    mid term code

---
+++ server/config/db.js
@@ -0,0 +1,11 @@
+// module.exports = {
+//   //local MongoDB deployment ->
+//   "URI": "mongodb://localhost/books229"
+// };
+
+module.exports = 
+{
+  
+    "URI": "mongodb+srv://Tahera:rOabUFNMh1aHAeNf@cluster0.c7vqo.mongodb.net/book-store?retryWrites=true&w=majority",
+    "Secret": 'SomeSecret'
+}
